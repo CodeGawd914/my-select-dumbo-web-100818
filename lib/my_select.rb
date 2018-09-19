7958020f11ce58e11858d8fa44a111b6b04e0a09
Travis Desir <travisgdesir31@gmail.com>
2018-09-19 21:24:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0 
+ collection = []
+ while i < array.length
+ collection.push yield (array[i])
+ i += 1
+ end
+ collection
 end
